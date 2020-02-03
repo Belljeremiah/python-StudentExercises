c154e7cfe06bb215709bf37fc6f84b564264a432
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2020-02-03 10:50:46 -0600

    work work work

---
+++ cohort.py
@@ -0,0 +1,6 @@
+class Cohort:
+    
+    def __init__(self):
+        self.name = ""
+        students = []
+        instructors = []
---
+++ student.py
@@ -0,0 +1,9 @@
+class Student:
+    
+    def__init__(self):
+        # Establishing the properties of each student
+        self.first_name = ""
+        self.last_name = ""
+        self.slack_handle = ""
+        self.cohort = ""
+        exercises = []
