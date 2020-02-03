9d388da0de8b7dd778c6efb7794c109d7637c20b
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2020-02-03 10:51:42 -0600

    instructor added

---
+++ instructor.py
@@ -0,0 +1,9 @@
+class Instructor:
+    
+    def __init__(self):
+        self.first_name = ""
+        self.last_name = ""
+        self.slack_handle = ""
+        cohort = []
+        self.specialty = ""
+        
